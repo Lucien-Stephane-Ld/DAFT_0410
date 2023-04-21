1a03b2827558ec9ed20e32565107defbf1f9315b
Lucien Stephane Ld <stephaneledan@gmail.com>
2023-04-21 10:32:42 +0200

    Lap_19_Ledan

--- Module_1/Lab_19_SQL-Finance/Lab_19_Ledan.sql
+++ Module_1/Lab_19_SQL-Finance/Lab_19_Ledan.sql
@@ -5,20 +5,74 @@ select max(price) as 'Max price', min(price) as 'Min price' from olist.order_ite
 
 -- 2. From the order_items table, what is range of the shipping_limit_date of the orders?
 
-select max(shipping_limit_date), min(shipping_limit_date)
+select datediff(max(shipping_limit_date),min(shipping_limit_date)) as 'Date difference'
 from olist.order_items;
 
 -- 3. From the customers table, find the states with the greatest number of customers.
 
-select max(customer_state) as 'State with most customers'
-from olist.customers;
+select customer_state, count(customer_unique_id)
+from olist.customers
+group by customer_state
+order by count(customer_unique_id) desc
+limit 10;
 
--- 4. From the customers table, within the state with the greatest number of customers, find the cities with the greatest number of customers.
+-- 4. From the customers table, within the state with the greatest number of customers,
+-- find the cities with the greatest number of customers.
 
-
+select distinct customer_city as 'City with most customers', count(customer_id)
+from olist.customers
+where customer_state = 'TO'
+group by customer_city
+order by count(customer_id) desc
+limit 10
+;
 
 -- 5. From the closed_deals table, how many distinct business segments are there (not including null)?
--- 6. From the closed_deals table, sum the declared_monthly_revenue for duplicate row values in business_segment and find the 3 business segments with the highest declared monthly revenue (of those that declared revenue).
--- 7. From the order_reviews table, find the total number of distinct review score values.
--- 8. In the order_reviews table, create a new column with a description that corresponds to each number category for each review score from 1 - 5, then find the review score and category occurring most frequently in the table.
+
+select business_segment as 'Business Segments', count(business_segment)
+FROM olist.closed_deals
+WHERE business_segment IS NOT NULL
+group by business_segment
+order by count(business_segment) desc
+;
+
+-- 6. From the closed_deals table, 
+-- sum the declared_monthly_revenue
+-- for duplicate row values in business_segment
+-- and find the 3 business segments with the highest declared monthly revenue (of those that declared revenue).
+
+select business_segment , sum(declared_monthly_revenue)
+from olist.closed_deals
+group by business_segment
+order by sum(declared_monthly_revenue) desc
+;
+
+-- 7. From the order_reviews table,    find the total number of     distinct review score     values.
+
+select distinct count(review_id)
+from olist.order_reviews
+;
+
+-- 8. In the order_reviews table, create a new column with a description that corresponds to each number category for each review score from 1 - 5,
+-- then find the review score and category occurring most frequently in the table.
+
+select review_score,
+        case when review_score = 1 then "Lame"
+             when review_score = 2 then "Meh"
+             when review_score = 3 then "Okay"
+             when review_score = 4 then "Good"
+             else "Great"
+        end as review_score_category 
+from olist.order_reviews
+group by review_score
+order by review_score desc;
+
+
 -- 9. From the order_reviews table, find the review value occurring most frequently and how many times it occurs.
+
+select review_score,  count(review_id)
+from olist.order_reviews
+group by review_score
+order by count(review_id) desc
+limit 10
+;
